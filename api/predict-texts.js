0d2119800f82239af1c05db12a52c88a10aee316
Christian Pezzin <christian.pezzin@gmail.com>
2025-07-10 10:09:15 +0200

    Create predict-texts.js

---
+++ api/predict-texts.js
@@ -0,0 +1,20 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
+
+export default async function handler(req, res) {
+  const { data, error } = await supabase
+    .from('predict_text')
+    .select('*')
+    .order('timestamp', { ascending: false });
+
+  if (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Errore durante il recupero dati.' });
+  }
+
+  res.status(200).json(data);
+}
